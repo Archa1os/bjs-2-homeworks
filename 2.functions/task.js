2c50bdf949ee7602096282ff5f0f96b9e30fcbe8
Archa1os <shooterdiox@gmail.com>
2022-07-17 12:28:33 +0400

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,18 +5,20 @@ function getArrayParams(arr) {
   let max = -Infinity;
   let sum = 0;
   let avg;
-  
-  for (let i = 0; i < arr.lenght; i++) {
+
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] < min) {
-      min = arr [i];
-      }
-    if (arr[i] > min){
-      max = arr [i];
+      min = arr[i];
     }
-    sum += arr [i];
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    sum += arr[i];
   }
 
-  avg = +(sum / arr.lenght).toFixed(2);
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
@@ -25,7 +27,7 @@ function getArrayParams(arr) {
 function worker(arr) {
   let sum = 0;
 
-  for (let i = 0; i < arr.lenght; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
 
@@ -35,30 +37,30 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
 
-  for (let i = 0; i < arrOfArr.lenght; i++) {
+  for (let i = 0; i < arrOfArr.length; i++) {
     if (max < func(arrOfArr[i])) {
       max = func(arrOfArr[i]);
     }
   }
-  
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let min = Infinity;
   let max = -Infinity;
+  let min = Infinity;
 
-  for (let i = 0; i <arr.lenght; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] < min) {
-      min = arr[i]
+      min = arr[i];
     }
 
-    if (arr[i]> max) {
-      max = arr [i];
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
-  let difference = math.abs(max - min);
+  let difference = Math.abs(max - min);
 
   return difference;
 }
