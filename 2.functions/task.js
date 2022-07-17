c500a523ea69c8d87fcaca96737c8714a14aed45
Archa1os <shooterdiox@gmail.com>
2022-07-17 11:53:18 +0400

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,66 +1,46 @@
 'use srtict'
 // Задание 1
 function getArrayParams(arr) {
+  let min, max, sum, avg;
   let min = Infinity;
   let max = -Infinity;
   let sum = 0;
   let avg;
-
-  for (let i = 0; i < arr.length; i++) {
+  
+  for (let i = 0; i < arr.lenght; i++) {
     if (arr[i] < min) {
-      min = arr[i];
+      min = arr [i];
+      }
+    if (arr[i] > min){
+      max = arr [i];
     }
-
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-
-    sum += arr[i];
+    sum += arr [i];
   }
 
-  avg = +(sum / arr.length).toFixed(2);
+  avg = +(sum / arr.lenght).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum = 0;
+  let sum;
 
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
+  // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max = -Infinity;
+  let max;
 
-  for (let i = 0; i < arrOfArr.length; i++) {
-    if (max < func(arrOfArr[i])) {
-      max = func(arrOfArr[i]);
-    }
-  }
-
+  // Ваш кода
+  // for ...
+  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let max = -Infinity;
-  let min = Infinity;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  let difference = Math.abs(max - min);
-
-  return difference;
+  // Ваш код
 }
