a16bfb2b5090b3a9b85df1a1687f575e6da68092
Archa1os <shooterdiox@gmail.com>
2022-07-24 13:01:54 +0400

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  every = (el,ind,arr) => el == arr2[ind] && arr.lenght == arr2.lenght;
+  result = arr1.every(every)
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(el => el >= 0).filter(el => el % 3 == 0 ).map( el => el * 10)
 
   return resultArr; // array
 }
